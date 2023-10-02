00165d41eb2e70b9211b1dc52e2697c0fe5f2ae9
Crystal-PuNK <228686187@qq.com>
2023-10-02 14:11:03 +0800

    - GY-86工程初始操作 - F401Nucleo64原理图批注

--- Firmware/GY-86/main.c
+++ Firmware/GY-86/main.c
@@ -60,37 +60,18 @@ int main(void)
   /* Insert 50 ms delay */
   Delay(5);
   
-  /* Output HSE clock on MCO1 pin(PA8) ****************************************/ 
+
   /* Enable the GPIOA peripheral */ 
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-  
-  /* Configure MCO1 pin(PA8) in alternate function */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;  
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-    
-  /* HSE clock selected to output on MCO1 pin(PA8)*/
-  RCC_MCO1Config(RCC_MCO1Source_HSE, RCC_MCO1Div_1);
-  
-  
-  /* Output SYSCLK/4 clock on MCO2 pin(PC9) ***********************************/ 
-  /* Enable the GPIOACperipheral */ 
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
   
-  /* Configure MCO2 pin(PC9) in alternate function */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;  
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-    
-  /* SYSCLK/4 clock selected to output on MCO2 pin(PC9)*/
-  RCC_MCO2Config(RCC_MCO2Source_SYSCLK, RCC_MCO2Div_4);
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
   
+  GPIO_SetBits(GPIOC,GPIO_Pin_13);
      
   /* Infinite loop */
   while (1)
